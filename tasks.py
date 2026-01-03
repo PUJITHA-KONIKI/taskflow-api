b7286573e85e1a1470d614e7af29022bbd80fc72
PUJITHA-KONIKI <pujithakoniki@gmail.com>
2026-01-03 14:52:46 +0530

    Update tasks.py

--- tasks.py
+++ tasks.py
@@ -1,5 +1,5 @@
-from fastapi import APIRouter, Depends
-from sqlalchemy.orm import Session
+from app.fastapi import APIRouter, Depends
+from app.sqlalchemy.orm import Session
 from app.database import get_db
 from app import schemas
 
