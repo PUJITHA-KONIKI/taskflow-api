f1de17f6084e4afe5bc45307b387aa48831e31b9
PUJITHA-KONIKI <pujithakoniki@gmail.com>
2026-01-03 13:55:58 +0530

    Refactor User class to use SQLAlchemy model

--- user.py
+++ user.py
@@ -1,6 +1,10 @@
-from pydantic import BaseModel
+from sqlalchemy import Column, Integer, String
+from app.database import Base
 
-class User(BaseModel):
-    email: str
-    password: str
-    role: str
+class User(Base):
+    __tablename__ = "users"
+
+    id = Column(Integer, primary_key=True, index=True)
+    email = Column(String, unique=True, index=True, nullable=False)
+    password = Column(String, nullable=False)
+    role = Column(String, default="user")
